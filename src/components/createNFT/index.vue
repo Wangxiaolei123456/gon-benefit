7654aa3dbe7ababb386c572cda2eecebf9ac1983
TheCodeor <447690182@qq.com>
2023-05-17 20:24:45 +0800

    创建页面UI ，sdk 完成

--- src/components/createNFT/index.vue
+++ src/components/createNFT/index.vue
@@ -1,72 +1,183 @@
 <template>
   <div class="container">
     <div class="topView">
-      <button class="btn"></button>
+      <button class="btn" @click="pushHome"></button>
       <div class="title">Create NFT</div>
     </div>
     <!-- <div class="scrollable">
 
     </div> -->
     <div class="create">
-        <input type="file" accept="image/*" ref="fileInput" style="display:none" @change="uploadFile">
-        <button class="addButton" @click="chooseFile"></button>
-        <div class="suggestedTitle">Suggested proportion 2:1 ( jpg/png/gif )</div>
-      </div>
-      <div class="name" style="padding-top: 35px;">
+      <input type="file" accept="image/*" ref="fileInput" style="display:none" @change="uploadFile">
+      <!-- <button class="addButton" @click="chooseFile"></button> -->
+      <img class="addButton" :src="loadeImageUrl(uploadedImageHash)" @click="chooseFile">
+      <div class="suggestedTitle">Suggested proportion 2:1 ( jpg/png/gif )</div>
+    </div>
+    <div class="name" style="padding-top: 35px;">
+      <div style="display: flex; justify-content: space-around;">
         <div class="title" style="text-align: left;">NFT Name</div>
-        <input class="textInput" type="text" v-model="inputNameText">
+        <div class="title" style="text-align: right;">{{ nameValue.length }}/80</div>
       </div>
-      <div class="description" style="padding-top: 25px;">
+      <input class="textInput" type="text" v-model="nameValue" maxlength="80">
+    </div>
+    <div class="description" style="padding-top: 25px;">
+      <div style="display: flex; justify-content: space-around;">
         <div class="title" style="text-align: left;">Description</div>
-        <textarea class="descriptionText" v-model="message"></textarea>
+        <div class="title" style="text-align: right;">{{ descriptionValue.length }}/800</div>
       </div>
-      <div class="name" style="padding-top: 25px;">
-        <div class="title" style="text-align: left;">Editions</div>
-        <input class="textInput" type="text" v-model="inputEditionsText">
-      </div>
-      <div style="width: 100%;">
-        <button class="subBtn" @click="submitButton">Submit</button>
-      </div>
+      <textarea class="descriptionText" v-model="descriptionValue" maxlength="800"></textarea>
+    </div>
+    <div class="name" style="padding-top: 25px;">
+      <div class="title" style="text-align: left;">Editions</div>
+      <input class="textInput" type="text" v-model="amountValue">
+    </div>
+    <div style="width: 100%;">
+      <button class="subBtn" @click="submitButton" :disabled="isInputEmpty">Submit</button>
+    </div>
+    <loading :isShowLoading="isShowLoading"></loading>
   </div>
 </template>
   
 <script>
-import {getMyBalance} from "/src/keplr/iris/wallet"
+import { getMyBalance, issueDenomAndMint, quiryTx, mintNFT } from "/src/keplr/iris/wallet"
+import { uploadImage, getNftImg } from "/src/api/image"
+import Loading from "@/components/loading.vue";
+
 export default {
   name: 'HelloWorld',
+  components: { Loading },
   data() {
     return {
-      inputNameText: '',// 初始化输入框的值为空
-      inputEditionsText: '',
-      message: ''
+      nameValue: '',// 初始化输入框的值为空
+      descriptionValue: '',
+      amountValue: '',
+      uploadedImageHash: 'QmPuuSpLdzV4Hz4aJtPUVzxsgnLKPYiqKdYtdTGyLF6Pn5',//默认的图片,
+      // nameValue: 'fantest',// 初始化输入框的值为空
+      // descriptionValue: 'fantest',
+      // amountValue: '',
+      // uploadedImageHash: 'QmTpb65U1hw46ieCwVq1MquCrwYDpwsPZdwwpo9jB8TAK2',//默认的图片,
+      isInputEmpty: true,
+      flag: true,
+      isShowLoading: false,
     }
   },
+  created() {
+    console.log(this.$store.state.IrisAddress)
+    console.log(this.nameValue)
+  },
+  watch: {
+    uploadedImageHash: 'checkInput',
+    nameValue: 'checkInput',
+    descriptionValue: 'checkInput',
+    amountValue: 'checkInput',
+  },
   methods: {
-    submitButton() {
-      // 在这里处理输入框的提交操作
-      console.log(this.inputText)
-      getMyBalance()
+    async submitButton() {
 
+      try {
+        console.log(this.nameValue)
+        this.isShowLoading = true
+
+        let name = this.nameValue;
+        let sender = 'iaa1wxl44399uppwd5uc6rrgz07plzs9atv8fxt7qr'
+        let data = ""
+        let amount = Number(this.amountValue)
+        let imageUrl = this.loadeImageUrl(this.uploadedImageHash)
+        console.log("wxl ---- mintNFT", name, sender, imageUrl, data, amount)
+
+        let txHash = await issueDenomAndMint(
+          name,
+          sender,
+          sender,
+          imageUrl,
+          data,
+          amount,
+        );
+        console.log(txHash)
+
+        await this.waitForTxConfirmation(txHash.txInfo.hash);
+
+        let title = "Create Success"
+        this.$mtip({
+          title: title,
+        });
+        this.isShowLoading = false
+        this.pushHome()
+
+      } catch (error) {
+        console.log(error);
+        // let title = "Create Success"
+        this.isShowLoading = false
+        this.$mtip({
+          title: error.message,
+        });
+      }
+    },
+
+    async waitForTxConfirmation(txHash) {
+      this.flag = true;
+      while (this.flag) {
+        console.log("wwwwww");
+        await this.sleep(5000);
+        let res = await quiryTx(txHash);
+        console.log("wxl -----  quiryTx");
+        console.log(res);
+        if (res.code == 0) {
+          this.flag = false;
+          return;
+        } else if (res.code == -1) {
+          this.flag = false;
+          throw new Error(res.log);
+        } else {
+          this.flag = true;
+        }
+      }
+    },
+    sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
     },
     chooseFile() {
       this.$refs.fileInput.click()
     },
-    uploadFile(event) {
+    async uploadFile(event) {
       console.log(event)
       const file = event.target.files[0]
       console.log(file)
       const formData = new FormData()
       formData.append('file', file)
-
-      // axios.post('/api/upload', formData, {
-      //   headers: {
-      //     'Content-Type': 'multipart/form-data'
-      //   }
-      // }).then(response => {
-      //   console.log(response.data)
-      // }).catch(error => {
-      //   console.log(error)
-      // })
+      try {
+        const value = await uploadImage(file);
+        console.log(value.data.data);
+        this.uploadedImageHash = value.data.data
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    loadeImageUrl(hash) {
+      return getNftImg(hash)
+    },
+    checkInput() {
+      this.checkAmountInput()
+      this.isInputEmpty =
+        this.nameValue.trim() === '' ||
+        this.descriptionValue.trim() === '' ||
+        this.amountValue.trim() === '' ||
+        this.uploadedImageHash.trim() === 'QmPuuSpLdzV4Hz4aJtPUVzxsgnLKPYiqKdYtdTGyLF6Pn5'
+      console.log(this.isInputEmpty)
+    },
+    checkAmountInput() {
+      let value = this.amountValue
+      console.log(value)
+      // 将非数字的字符替换为空字符串
+      value = value.replace(/[^\d]/g, '')
+      // 将大于50的数字替换为50
+      if (value > 50) {
+        value = '50'
+      }
+      this.amountValue = value
+    },
+    pushHome() {
+      this.$router.push('/')
     }
   }
 }
@@ -89,6 +200,7 @@ export default {
   width: 100%;
   overflow: scroll;
 }
+
 ::-webkit-scrollbar {
   display: none;
 }
@@ -130,13 +242,14 @@ export default {
 
 .addButton {
   background-color: #fb599b;
-  background-image: url('/src/assets/icon_add.png');
+  // background-image: url('/src/assets/icon_add.png');
   background-size: 300px;
   background-position: center;
   border: none;
   height: 175px;
   width: 100%;
   border-radius: 5px;
+  object-fit: contain;
 }
 
 .suggestedTitle {
@@ -191,17 +304,20 @@ export default {
   margin-top: 35px;
   // margin-bottom: 20px;
   width: 100%;
-	height: 41px;
-	background-color: #fb599b;
-	border-radius: 20px;
+  height: 41px;
+  background-color: #fb599b;
+  border-radius: 20px;
 
-	font-family: AmpleSoft-Bold !important;
-	font-size: 15px !important;
-	font-weight: normal;
-	font-stretch: normal;
-	letter-spacing: 0px;
-	color: #ffffff;
+  font-family: AmpleSoft-Bold !important;
+  font-size: 15px !important;
+  font-weight: normal;
+  font-stretch: normal;
+  letter-spacing: 0px;
+  color: #ffffff;
 }
 
+.subBtn:disabled {
+  background-color: gray;
+}
 </style>
   
--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import router from "./router";
 import store from "./store";
 import vuetify from './plugins/vuetify'
 import './assets/font/font.css'
-global.Buffer = global.Buffer || require('buffer').Buffer;
 
+import messageTip from "./components/MessageTip"
+messageTip(Vue);
 // import {initWallet  } from "./wallet/index";
 // initWallet();
+global.Buffer = global.Buffer || require('buffer').Buffer;
 
 Vue.config.productionTip = false
 
