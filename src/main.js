eff9c5acc1ef95283fd2a302c3e99c01f20860ed
wangxiaolei <wangxiaolei@897672818@qq.com>
2023-05-17 14:42:46 +0800

    update store data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from "./router";
+import store from "./store";
 import vuetify from './plugins/vuetify'
 import './assets/font/font.css'
 
@@ -11,6 +12,7 @@ Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  store,
   router,
   render: h => h(App),
 }).$mount('#app')
